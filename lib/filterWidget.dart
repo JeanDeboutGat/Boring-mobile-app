6514d5faa97e3af1456af16b2d9766a5a72413b6
JeanDeboutGat <jeandebout.gatari.etu@univ-lille.fr>
2022-11-26 14:02:35 +0100

    feat(filter): add selection filter and enhance some codes.

--- lib/filterWidget.dart
+++ lib/filterWidget.dart
@@ -1,12 +1,34 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 import 'CustomAppBar.dart';
 import 'constants.dart';
 
+class ActivityType {
+  String type;
+  String image;
+
+  ActivityType(this.type, this.image);
+
+  @override
+  bool operator ==(Object other) =>
+      other is ActivityType && (other.type == type && other.image == image);
+
+  @override
+  int get hashCode => type.hashCode;
+}
+
 class FilterWidget extends StatefulWidget {
-  const FilterWidget({super.key});
+  FilterWidget({super.key});
+
+  final List<ActivityType> typesList = <ActivityType>[
+    ActivityType("education", "assets/cost-highlighted.png"),
+    ActivityType("recreational", "assets/cost-highlighted.png"),
+    ActivityType("charity", "assets/cost-highlighted.png"),
+    ActivityType("relaxation", "assets/cost-highlighted.png"),
+    ActivityType("music", "assets/cost-highlighted.png"),
+    ActivityType("busywork", "assets/cost-highlighted.png"),
+  ];
 
   @override
   State<StatefulWidget> createState() {
@@ -15,15 +37,23 @@ class FilterWidget extends StatefulWidget {
 }
 
 class _FilterWidgetState extends State<FilterWidget> {
-  double _currentSliderValue = 20;
+  double _currentPriceSliderValue = 20;
+  double _currentAccessibilitySliderValue = 20;
+  late ActivityType selectedTypeValue;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedTypeValue = widget.typesList.first;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(true, false, preferredSize: Size(56, 56)),
+      appBar: const CustomAppBar(true, false, preferredSize: Size(56, 56)),
       backgroundColor: const Color(COLORS.accent),
       body: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
         child: Column(
           children: [
             const Padding(
@@ -37,9 +67,9 @@ class _FilterWidgetState extends State<FilterWidget> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Participants"),
+                const Text("Participants"),
                 ToggleSwitch(
-                  customWidths: [50.0, 50.0],
+                  customWidths: const [50.0, 50.0],
                   cornerRadius: 20.0,
                   activeBgColors: const [
                     [Color(COLORS.secondary)],
@@ -48,7 +78,7 @@ class _FilterWidgetState extends State<FilterWidget> {
                   activeFgColor: Colors.white,
                   inactiveBgColor: Colors.white,
                   totalSwitches: 2,
-                  icons: [Icons.person, Icons.group],
+                  icons: const [Icons.person, Icons.group],
                   onToggle: (index) {
                     print('switched to: $index');
                   },
@@ -58,25 +88,26 @@ class _FilterWidgetState extends State<FilterWidget> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Price range"),
+                const Text("Price range"),
                 Row(
                   children: [
-                    Slider(
-                      value: _currentSliderValue,
-                      max: 100,
-                      divisions: 5,
-                      activeColor: Color(COLORS.secondary),
-                      inactiveColor: Color(COLORS.primary),
-                      label: _currentSliderValue.round().toString(),
-                      onChanged: (double value) {
-                        setState(() {
-                          _currentSliderValue = value;
-                        });
-                      },
+                    Expanded(
+                      child: Slider(
+                        value: _currentPriceSliderValue,
+                        max: 100,
+                        divisions: 100,
+                        activeColor: const Color(COLORS.primary),
+                        inactiveColor: const Color(COLORS.secondary),
+                        label: _currentPriceSliderValue.round().toString(),
+                        onChanged: (double value) {
+                          setState(() {
+                            _currentPriceSliderValue = value;
+                          });
+                        },
+                      ),
                     ),
-                    Image(
-                        image: AssetImage(
-                            "assets/accessibility-highlighted.png")),
+                    const Image(
+                        image: AssetImage("assets/cost-highlighted.png")),
                   ],
                 )
               ],
@@ -84,30 +115,96 @@ class _FilterWidgetState extends State<FilterWidget> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Accessibility"),
+                const Text("Accessibility"),
                 Row(
                   children: [
-                    Slider(
-                      value: _currentSliderValue,
-                      max: 100,
-                      divisions: 5,
-                      activeColor: Color(COLORS.secondary),
-                      inactiveColor: Color(COLORS.primary),
-                      label: _currentSliderValue.round().toString(),
-                      onChanged: (double value) {
-                        setState(() {
-                          _currentSliderValue = value;
-                        });
-                      },
+                    Expanded(
+                      child: Slider(
+                        value: _currentAccessibilitySliderValue,
+                        max: 100,
+                        divisions: 100,
+                        activeColor: const Color(COLORS.primary),
+                        inactiveColor: const Color(COLORS.secondary),
+                        label:
+                            _currentAccessibilitySliderValue.round().toString(),
+                        onChanged: (double value) {
+                          setState(() {
+                            _currentAccessibilitySliderValue = value;
+                          });
+                        },
+                      ),
                     ),
-                    Image(
-                        image: AssetImage(
-                            "assets/accessibility-highlighted.png")),
+                    const Image(
+                        image:
+                            AssetImage("assets/accessibility-highlighted.png")),
                   ],
                 ),
-
               ],
-            )
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Chosen Type"),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusColor: const Color(COLORS.secondary),
+                    child: DropdownButton<ActivityType>(
+                      isExpanded: true,
+                      value: selectedTypeValue,
+                      icon: const Icon(Icons.arrow_drop_down_outlined),
+                      iconSize: 42,
+                      elevation: 16,
+                      underline: const SizedBox(),
+                      onChanged: (dynamic? newValue) {
+                        setState(() {
+                          print("chosen type: $newValue");
+                          selectedTypeValue = newValue!;
+                        });
+                      },
+                      items: widget.typesList
+                          .map<DropdownMenuItem<ActivityType>>(
+                              (ActivityType value) {
+                        return DropdownMenuItem<ActivityType>(
+                          value: value,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(value.type),
+                              const Image(image: AssetImage("assets/cook.png")),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  FloatingActionButton.extended(
+                    onPressed: () {
+                      // Add your onPressed code here!
+                    },
+                    label: const Text('Generate random activities'),
+                    backgroundColor: const Color(COLORS.primary),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
