1139e674c8b4cd98b1cc4087ec642cd526fc97ec
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2023-01-14 18:48:15 +0100

    feat: add quick category filter using BloC (#1)

--- lib/services/boring-api.service.dart
+++ lib/services/boring-api.service.dart
@@ -6,8 +6,12 @@ import 'package:me_bored/models/activity.model.dart';
 class BoringApi {
   String apiBaseUrl = "https://www.boredapi.com/api/";
 
-  Future<Activity> getRandomActivity() async {
-    http.Response response = await http.get("${apiBaseUrl}activity");
+  Future<Activity> getRandomActivity({String? category}) async {
+    var url = "${apiBaseUrl}activity";
+    if (category != null && category != "") {
+      url += "?type=$category";
+    }
+    http.Response response = await http.get(url);
     if (response.statusCode == 200) {
       var data = json.decode(response.body);
       return Activity(data['activity'], Activity.fromTypeActivityString(data['type']), Activity.fromParticipantsNumber(data['participants']), data['price'].toDouble(), data['accessibility'].toDouble());
